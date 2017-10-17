d0ffd2695529a75a22be21a672634618930acbbd
TainaLepisto <lepisto.taina@gmail.com>
2017-10-17 16:59:34 +0300

    Laurin YLE projektipohjalle

---
+++ app/scripts/views/Player.js
@@ -0,0 +1,44 @@
+import {bind} from 'hyperHTML';
+
+/**
+ * The HyperHTML view displaying the video player and channel details.
+ */
+export default class Player {
+  /**
+   * Default constructor for setting the values
+   *
+   * @param {HTMLElement} element - The HTML element to bind/adopt
+   * @param {String} [url=null] - The URL to use
+   * @param {Object} [program=null] - The program to render
+   */
+  constructor(element, url=null, program=null) {
+    this.element = element;
+    this.url = url;
+    this.program = program;
+  }
+
+  /**
+   * Renders the player.
+   *
+   * @return {HTMLElement} The rendered element
+   */
+  render() {
+    console.log(`Render Player with url '${this.url}'`);
+    const start = this.program.startTime;
+    const time = `${ start.getHours() }:${ String(start.getMinutes()).padStart(2, '0') }`;
+
+    return bind(this.element)`
+      <video autoplay controls width="100%">
+        <source src="${this.url}" type="application/x-mpegURL">
+        Selaimesi ei tue videosoittoa.
+      </video>
+      <div class="mdc-card">
+        <section class="mdc-card__primary">
+          <h2 class="mdc-card__subtitle">${ time }</h2>
+          <h1 class="mdc-card__title mdc-card__title--large">${ this.program.title }</h1>
+        </section>
+        <section class="mdc-card__supporting-text">${ this.program.description }</section>
+      </div>
+    `;
+  }
+}
---
+++ gulpfile.babel.js
@@ -0,0 +1,291 @@
+/**
+ *
+ *  Web Starter Kit
+ *  Copyright 2015 Google Inc. All rights reserved.
+ *
+ *  Licensed under the Apache License, Version 2.0 (the 'License');
+ *  you may not use this file except in compliance with the License.
+ *  You may obtain a copy of the License at
+ *
+ *      https://www.apache.org/licenses/LICENSE-2.0
+ *
+ *  Unless required by applicable law or agreed to in writing, software
+ *  distributed under the License is distributed on an 'AS IS' BASIS,
+ *  WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ *  See the License for the specific language governing permissions and
+ *  limitations under the License
+ *
+ */
+
+'use strict';
+
+// This gulpfile makes use of new JavaScript features.
+// Babel handles this without us having to do anything. It just works.
+// You can read more about the new JavaScript features here:
+// https://babeljs.io/docs/learn-es2015/
+import {output as pagespeed} from 'psi';
+import BabelMinifyPlugin from 'babel-minify-webpack-plugin';
+import browserSync from 'browser-sync';
+import del from 'del';
+import gulp from 'gulp';
+import gulpLoadPlugins from 'gulp-load-plugins';
+import path from 'path';
+import pkg from './package.json';
+import polyfill from 'babel-polyfill';
+import pushHandler from './api/subscriptions';
+import runSequence from 'run-sequence';
+import webpack from 'webpack';
+import webpackStream from 'webpack-stream';
+import workboxBuild from 'workbox-build';
+
+const $ = gulpLoadPlugins();
+const reload = browserSync.reload;
+
+// Lint JavaScript
+gulp.task('lint', () =>
+  gulp.src(['app/scripts/**/*.js', '!node_modules/**'])
+    .pipe($.eslint())
+    .pipe($.eslint.format())
+    .pipe($.if(!browserSync.active, $.eslint.failAfterError()))
+);
+
+// Optimize images
+gulp.task('images', () =>
+  gulp.src('app/images/**/*')
+    .pipe($.cache($.imagemin({
+      progressive: true,
+      interlaced: true,
+    })))
+    .pipe(gulp.dest('dist/images'))
+    .pipe($.size({title: 'images'}))
+);
+
+// Copy all files at the root level (app)
+gulp.task('copy', () =>
+  gulp.src([
+    'app/*',
+    '!app/*.html',
+    'node_modules/apache-server-configs/dist/.htaccess',
+  ], {
+    dot: true,
+  }).pipe(gulp.dest('dist'))
+    .pipe($.size({title: 'copy'}))
+);
+
+// Compile and automatically prefix stylesheets
+gulp.task('styles', () => {
+  const AUTOPREFIXER_BROWSERS = [
+    'ie >= 10',
+    'ie_mob >= 10',
+    'ff >= 30',
+    'chrome >= 34',
+    'safari >= 7',
+    'opera >= 23',
+    'ios >= 7',
+    'android >= 4.4',
+    'bb >= 10',
+  ];
+
+  // For best performance, don't add Sass partials to `gulp.src`
+  return gulp.src([
+    // Just pick the main SASS files
+    'app/styles/main.scss',
+  ])
+    .pipe($.newer('.tmp/styles'))
+    .pipe($.sourcemaps.init())
+    .pipe($.sass({
+      precision: 10,
+      includePaths: ['node_modules'],
+    }).on('error', $.sass.logError))
+    .pipe($.autoprefixer(AUTOPREFIXER_BROWSERS))
+    .pipe(gulp.dest('.tmp/styles'))
+    // Concatenate and minify styles
+    .pipe($.if('*.css', $.cssnano()))
+    .pipe($.size({title: 'styles'}))
+    .pipe($.sourcemaps.write('./'))
+    .pipe(gulp.dest('dist/styles'))
+    .pipe(gulp.dest('.tmp/styles'));
+});
+
+// Webpack all the contents
+gulp.task('scripts', () => {
+  const webpackSettings = {
+    cache: true,
+    devtool: 'source-map',
+    entry: {
+      // Use entry name as the output file name to gulp pick up the file name
+      main: ['babel-polyfill', './app/scripts/main.js'],
+    },
+    output: {
+      filename: 'bundle.js',
+    },
+    resolve: {
+      extensions: ['.js', '.jsx'],
+    },
+    module: {
+      rules: [
+        {
+          loader: 'babel-loader',
+          test: /\.(js|jsx)$/,
+          exclude: [
+            /node_modules\/proptypes|scripts\/sw.js/,
+          ],
+        },
+      ],
+    },
+    plugins: [
+      new webpack.optimize.AggressiveMergingPlugin(),
+      new BabelMinifyPlugin({}, {}),
+    ],
+  };
+
+  return webpackStream(webpackSettings)
+    .pipe(gulp.dest('.tmp/scripts'))
+    .pipe($.size({title: 'scripts'}))
+    .pipe(gulp.dest('dist/scripts'))
+    .pipe(gulp.dest('.tmp/scripts'));
+});
+
+// Scan your HTML for assets & optimize them
+gulp.task('html', () => {
+  return gulp.src('app/**/*.html')
+    .pipe($.useref({
+      searchPath: '{.tmp,app}',
+      noAssets: true,
+    }))
+
+    // Minify any HTML
+    .pipe($.if('*.html', $.htmlmin({
+      removeComments: true,
+      collapseWhitespace: true,
+      collapseBooleanAttributes: true,
+      removeAttributeQuotes: true,
+      removeRedundantAttributes: true,
+      removeEmptyAttributes: true,
+      removeScriptTypeAttributes: true,
+      removeStyleLinkTypeAttributes: true,
+      removeOptionalTags: true,
+    })))
+    // Output files
+    .pipe($.if('*.html', $.size({title: 'html', showFiles: true})))
+    .pipe(gulp.dest('dist'));
+});
+
+// Clean output directory
+gulp.task('clean', () => del(['.tmp', 'dist/*', '!dist/.git'], {dot: true}));
+
+// Watch files for changes & reload
+gulp.task('serve', ['scripts', 'styles'], () => {
+  browserSync({
+    notify: false,
+    // Customize the Browsersync console logging prefix
+    logPrefix: 'WSK',
+    // Allow scroll syncing across breakpoints
+    scrollElementMapping: ['main'],
+    // Run as an https by uncommenting 'https: true'
+    // Note: this uses an unsigned certificate which on first access
+    //       will present a certificate warning in the browser.
+    // https: true,
+    server: ['.tmp', 'app', 'dist'],
+    // https: true,
+    // Custom PUSH backend handler
+    middleware: [
+      require('body-parser').json(),
+      {
+        route: '/api/subscriptions',
+        handle: pushHandler,
+      },
+    ],
+    port: 3000,
+  });
+
+  gulp.watch(['app/**/*.html'], reload);
+  gulp.watch(['app/styles/**/*.{scss,css}'], ['styles', reload]);
+  gulp.watch(['app/scripts/**/*.js'], ['lint', 'scripts', reload]);
+  gulp.watch(['app/images/**/*'], reload);
+});
+
+// Build and serve the output from the dist build
+gulp.task('serve:dist', ['default'], () =>
+  browserSync({
+    notify: false,
+    logPrefix: 'WSK',
+    // Allow scroll syncing across breakpoints
+    scrollElementMapping: ['main'],
+    // Run as an https by uncommenting 'https: true'
+    // Note: this uses an unsigned certificate which on first access
+    //       will present a certificate warning in the browser.
+    // https: true,
+    server: 'dist',
+    // https: true,
+    // Custom PUSH backend handler
+    middleware: [
+      require('body-parser').json(),
+      {
+        route: '/api/subscriptions',
+        handle: pushHandler,
+      },
+    ],
+    /* key: './certs/192.168.1.101.key',
+    cert: './certs/192.168.1.101.crt',*/
+    port: 3001,
+  })
+);
+
+// Build production files, the default task
+gulp.task('default', ['clean'], (cb) =>
+  runSequence(
+    'styles',
+    ['lint', 'html', 'scripts', 'images', 'copy'],
+    'generate-service-worker',
+    cb
+  )
+);
+
+// Run PageSpeed Insights
+gulp.task('pagespeed', (cb) =>
+  // Update the below URL to the public URL of your site
+  pagespeed('example.com', {
+    strategy: 'mobile',
+    // By default we use the PageSpeed Insights free (no API key) tier.
+    // Use a Google Developer API key if you have one: http://goo.gl/RkN0vE
+    // key: 'YOUR_API_KEY'
+  }, cb)
+);
+
+// Copy the workbox script and the prefetching module
+gulp.task('copy-workbox', () => {
+  return gulp.src([
+    'node_modules/workbox-sw/build/importScripts/workbox-sw.prod.*.js',
+    'node_modules/workbox-runtime-caching/build/importScripts/workbox-runtime-caching.prod.*.js',
+  ])
+  .pipe($.if(/workbox-sw/, $.rename('workbox.js')))
+  .pipe($.if(/workbox-runtime-caching/, $.rename('workbox-runtime-caching.js')))
+  .pipe(gulp.dest('dist/scripts/service-worker'));
+});
+
+// See http://www.html5rocks.com/en/tutorials/service-worker/introduction/ for
+// an in-depth explanation of what service workers are and why you should care.
+// Generate a service worker file that will provide offline functionality for
+// local resources. This should only be done for the 'dist' directory, to allow
+// live reload to work as expected when serving from the 'app' directory.
+gulp.task('generate-service-worker', ['copy', 'copy-workbox'], () => {
+  // Note: We use a partial manifest where we inject the built files.
+  return workboxBuild.injectManifest({
+    globDirectory: './dist/',
+    globPatterns: ['**\/*'],
+    globIgnores: ['.htaccess', 'robots.txt', 'humans.txt'],
+    swSrc: './app/service-worker.js',
+    swDest: './dist/service-worker.js',
+  })
+  .then(() => {
+    console.log('Service worker generated.');
+  })
+  .catch((err) => {
+    console.log('[ERROR] This happened: ' + err);
+  });
+});
+
+// Load custom tasks from the `tasks` directory
+// Run: `npm install --save-dev require-dir` from the command-line
+// try { require('require-dir')('tasks'); } catch (err) { console.error(err); }
